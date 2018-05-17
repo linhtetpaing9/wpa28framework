0c378dd1c4c2f51c77242c50dc9a9693f4ded436
Lin Htet Paing <linhtetpaing9@gmail.com>
2018-05-17 14:56:05 +0630

    Nothing Special

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,192 +1,94 @@
-@extends('layouts.dashboard')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
 
+<head>
+    @include('layouts.title')
+    @include('layouts.css')
+</head>
 
-@section('css')
+<body class="fix-header fix-sidebar">
+    <!-- Preloader - style you can find in spinners.css -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+        </div>
+        <div id="main-wrapper">
 
-<link rel="stylesheet" href="{{asset('css/select2.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/select2-bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/pretty-checkbox.min.css')}}">
-<link href="{{ asset('css/mediamanager.css') }}" rel="stylesheet">
+            <div class="unix-login">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4">
+                            <div class="login-content card">
+                                <div class="login-form">
+                                    <h4>Register</h4>
+                                    <form  class="form-horizontal" method="POST" action="{{ route('register') }}">
+                                        {{ csrf_field() }}
+                                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                            <label for="name">Name</label>
+                                            
+                                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-@endsection
+                                                @if ($errors->has('name'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                                @endif
+                                       
+                                        </div>
+                                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                            <label for="email" >E-Mail Address</label>
+                                            
+                                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-@section('title')
+                                                @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                                @endif
+                                           
+                                        </div>
+                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label>Password</label>
+                                            
+                                                <input id="password" type="password" class="form-control" name="password" required>
 
-<h3 class="text-primary">User Create Form</h3>
+                                                @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                                @endif
+                                            
+                                        </div>
 
-@endsection
+                                        <div class="form-group">
+                                            <label for="password-confirm">Confirm Password</label>
 
-
-
-@section('content')
-<div class="col-12">
-    <div class="card">
-        <div class="card-body"> 
-            <div class="col-lg-10 mx-auto">
-
-                <form action="{{route('user.store')}}" method="POST" class="form-horizontal form-bordered">
-                    {{csrf_field()}}
-                    <div class="form-body">
-                        <div class="form-group row {{ $errors->has('name') ? ' has-error' : '' }}">
-                            <div class="col-md-3">
-                                <label class="control-label text-right">Name</label>
-                            </div>
-                            <div class="col-md-9">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('name') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-
-
-                        <div class="form-group row{{ $errors->has('profile_image') ? ' has-error' : ''}}">
-                            <div class="col-md-3">
-                                <label class="control-label text-right">Profile Image</label>
-                            </div>
-                            <div class="col-md-9">
-                                <div id="upload"> 
-
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" name="profile_image" id="page_image" alt="Image thumbnail" placeholder="Profile Image" v-model="pageImage">
-                                        <span>
-                                            <button type="button" class="btn btn-primary" @click="showMediaManager = true">Select Image</button>
-                                        </span>
-                                    </div>
-
-
-                                    <media-modal v-if="showMediaManager" @media-modal-close="showMediaManager = false">
-                                        <media-manager
-                                        :is-modal="true"
-                                        selected-event-name="editor"
-                                        @media-modal-close="showMediaManager = false"
-                                        >
-                                    </media-manager>
-                                </media-modal>
-                            </div>
-
-                            @if ($errors->has('profile_image'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('profile_image') }}</strong>
-                            </span>
-                            @endif
-                        </div>
-                    </div>
-
-
-
-                    <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <div class="col-md-3">
-                            <label class="control-label text-right">Email</label>
-                        </div>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                            @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                            @endif
-                        </div>
-                    </div>
-
-
-
-
-
-
-
-                    <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <div class="col-md-3">
-                            <label class="control-label text-right">Password</label>
-                        </div>
-                        <div class="col-md-9">
-                            <input type="password" class="form-control" name="password" required>
-
-                            @if ($errors->has('password'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                            @endif
-                        </div>
-                    </div>
-
-
-                    <div class="form-group row">
-                        <div class="col-md-3">
-                            <label class="control-label text-right">Confirm Password</label>
-                        </div>
-                        <div class="col-md-9">
-                            <input type="password" class="form-control" name="password_confirmation" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-md-3">
-                            <i class="fas fa-user-secret" style="font-size: 2.5em"></i>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="pretty p-default" style="margin-top: 14px">
-                                <input type="checkbox" name="is_admin" value='true'/>
-                                <div class="state p-primary">
-                                    <label>SuperAdmin</label>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-
-                    <div class="form-group row">
-                        <div class="col-md-3">
-                            <label class="control-label text-right">Role</label>
-                        </div>
-
-                        <div class="col-md-6" style="padding-top: 10px">
-                            <select class="form-control myclass" name="role_id" single="single" >
-                                @foreach($role as $key=>$value)
-                                <option value="{{ $key }}">{{ $value }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-
-
-                </div>
-                <div class="form-actions">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="row">
-                                <div class="offset-sm-3 col-md-9">
-                                    <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Submit</button>
-                                    <button type="button" class="btn btn-inverse">Cancel</button>
+                                           
+                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                            
+                                        </div>
+                                        <div class="form-group">
+                                            
+                                                <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">
+                                                    Register
+                                                </button>
+                                            
+                                        </div>
+                                        
+                                        <div class="register-link m-t-15 text-center">
+                                            <p>Already have account ? <a href="#"> Sign in</a></p>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </form>
+            </div>
 
         </div>
+        @include('layouts.script')
 
-    </div>
-</div>
-</div>
+    </body>
 
-@endsection
-
-@section('script')
-<script src="{{ asset('js/mediamanager.js') }}"></script>
-<script src="{{asset('js/select2.min.js')}}"></script>
-<script>
-    $("document").ready(function(){
-        $(".myclass").select2();
-    });
-</script>
-
-@endsection
+    </html>
